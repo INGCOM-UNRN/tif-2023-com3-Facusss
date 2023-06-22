c96641c3f06ac4eddf165eb061c3da2bc60f8670
Facusss <facundocrq@gmail.com>
2023-06-21 21:53:26 -0300

    Actualizacion de EDG

--- main.py
+++ main.py
@@ -1,24 +1,28 @@
 from func_globales import numero_valido as num_val
-from clasica import Calculadora_Clasica
+from clasica import Calculadora_Clasica 
 from fraccion import Calculadora_fracciones
 from conversion import Calculadora_Conversion
 
 print("""
     Para una mejor experiencia le sugerimos 
         expandir el tamaño de la terminal
-    
+    """)
+opcion= 0
+
+
+while opcion !=4:
+
+
+    print("""   
         ---- MENU CALCULADORAS ----
 
                 1 - Clasica
                 2 - Fracciones 
                 3 - Conversiones
                 4 - Salir / off
+        """)
 
-    """)
-opcion= num_val( input("     Ingrese el numero de calculadora:  ") )
-
-
-while opcion !=4:
+    opcion= num_val( input("     Ingrese el numero de calculadora:  ") )
 
     while opcion<=0 or opcion>=5:
         print("\n    Opción no válida, ingrese nuevamente. \n")
@@ -33,16 +37,8 @@ while opcion !=4:
     elif opcion == 3:
         Calculadora_Conversion()
         
-    print("""   
-        ---- MENU CALCULADORAS ----
+    
 
-                1 - Clasica
-                2 - Fracciones 
-                3 - Conversiones
-                4 - Salir / off
-    """)
-
-    opcion= num_val( input("     Ingrese el numero de calculadora:  ") )
 
 
 print("""
